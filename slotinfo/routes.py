8e2bf0a086f4fea20961818d983e68f679269748
akash.chakraborty <akash.chakraborty@irdeto.com>
2021-12-27 17:59:29 +0530

    Project Add functionality and redirect added

--- slotinfo/routes.py
+++ slotinfo/routes.py
@@ -1,9 +1,10 @@
 from slotinfo import app
-from flask import render_template
+from flask import render_template,request,redirect
 from slotinfo.models import Item
 from slotinfo.models import Projects
-from slotinfo.forms import ReserveForm
-
+from slotinfo.forms import AddProjectForm
+from slotinfo import db
+from slotinfo.models import Projects
 
 # @app.route('/')					# The decorator which specifies the url we are going to navigate to
 
@@ -22,27 +23,42 @@ def about_page():
 @app.route('/')	
 @app.route('/home')
 def home_page():
-	return render_template('home.html')
+	form = AddProjectForm()
+	return render_template('home.html',form=form)
 
 @app.route('/rack1')
 def rack_one():
 	items = Item.query.filter_by(rack_number=1)
-	return render_template('rack1.html',items=items)
+	form = AddProjectForm()
+	return render_template('rack1.html',items=items,form=form)
 
 @app.route('/rack2')
 def rack_two():
 	items2 = Item.query.filter_by(rack_number=2)
-	return render_template('rack2.html',items=items2)
+	form = AddProjectForm()
+	return render_template('rack2.html',items=items2,form=form)
 
 @app.route('/rack3')
 def rack_three():
 	items3 = Item.query.filter_by(rack_number=3)
-	return render_template('rack3.html',items=items3)
+	form = AddProjectForm()
+	return render_template('rack3.html',items=items3,form=form)
 
 @app.route('/witbe1')
 def wit_rack1():
 	items4 = Item.query.filter_by(rack_number=4)
-	return render_template('rack4.html',items=items4)
+	form = AddProjectForm()
+	return render_template('rack4.html',items=items4,form=form)
+
+@app.route('/addproject', methods=['GET','POST'])
+def add_project():
+	form = AddProjectForm()
+	name=form.projectname
+	name2 = request.form['projectname']
+	x=Projects(projname=name2)
+	db.session.add(x)
+	db.session.commit()
+	return redirect(request.referrer)
 
 # @app.route('/signup/official')
 # def signup_page1():
